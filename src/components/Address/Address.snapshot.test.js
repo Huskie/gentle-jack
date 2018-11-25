829317da7dd39d358b2c506ca544d0fe9c526b7b
Darren Huskie <darren@darrenhuskie.com>
2018-11-25 01:17:06 +0000

    Added first snapshot, and babel config

---
+++ src/components/Address/Address.snapshot.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { Address } from './Address';
+
+describe('Address snapshots', () => {
+    it('component renders correctly', () => {
+        const tree = renderer.create(<Address>
+            <span>Registered office address:</span>
+            <span>272 Bath Street</span>
+            <span>Glasgow, G2 4JR</span>
+        </Address>).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
